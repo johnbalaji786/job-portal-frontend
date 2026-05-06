ccf3128954f9ed1fbc691f0e2a8429b01048f98b
Blaji <johnbalaji786@gmail.com>
2026-05-06 14:10:03 +0530

    toast setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router"
+import { ToastContainer } from "react-toastify";
+import Home from "./Pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <RouterProvider router={router} />
+     <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  
   )
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import { toast } from "react-toastify"
+
+const Home = () => {
+
+    toast.success("Welcome to Job Portal");
+    
+    return (
+
+      <>
+      <div>Home</div>
+      </>
+    
+  )
+}
+
+export default Home
